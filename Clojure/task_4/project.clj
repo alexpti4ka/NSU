cb5029586e984b8652a876899c01b3d1d8d256e3
Elena Ptitsyna <alexpti4ka@gmail.com>
2025-10-16 12:06:55 +0700

    chore: Clean repository for new structure

--- Clojure/task_4/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject task_4 "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [compojure "1.6.2"]
-                 [ring/ring-core "1.9.0"]
-                 [ring/ring-jetty-adapter "1.9.0"]]
-  :main task_4_api)  ; Запуск АПИ
